e1cd00bdf7de8185a5ed6dd4092ed9b3904ba7ce
José Rosendo <rosendojose0617@gmail.com>
2021-10-07 13:37:09 +0000

    feat: Create model pet with their migration and show the first pet in hello view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ActionController::Base
     def hello
-        @title = 'Hola mundo!'
+        first_pet = Pet.first
+
+        @title = "Hola, soy #{first_pet.name} y soy un #{first_pet.breed}"
     end
 end
